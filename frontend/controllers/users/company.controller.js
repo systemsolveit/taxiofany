364f2151d1f1cd0c5d40ef0e2c8ca2ffbb1c2d50
KhamisOO <elkhamisadel@gmail.com>
2026-04-06 18:02:52 +0200

    Load public fleet data and use view partials
    Add a publicFleetData service to centralize loading cars and drivers (loadPublicFleetData) and helper to split cars across pricing tabs (splitCarsForPricingTabs). Update company and public controllers to async handlers that fetch fleet data and pass cars/drivers/pricingTabs into views (with proper error forwarding). Replace hardcoded driver and vehicle markup with new EJS partials (driver-team-item, pricing-car-item) and update about-us/about-company, home/index and home/modern templates to iterate data and show friendly fallbacks when no items are published. Also ensure links respect withLocale where available.

--- frontend/controllers/users/company.controller.js
+++ frontend/controllers/users/company.controller.js
@@ -1,7 +1,19 @@
-exports.aboutUs = (req, res) => {
-  res.render('users/company/about-us');
+const { loadPublicFleetData } = require('../../services/publicFleetData');
+
+exports.aboutUs = async (req, res, next) => {
+  try {
+    const { drivers } = await loadPublicFleetData();
+    return res.render('users/company/about-us', { drivers });
+  } catch (error) {
+    return next(error);
+  }
 };
 
-exports.aboutCompany = (req, res) => {
-  res.render('users/company/about-company');
+exports.aboutCompany = async (req, res, next) => {
+  try {
+    const { drivers } = await loadPublicFleetData();
+    return res.render('users/company/about-company', { drivers });
+  } catch (error) {
+    return next(error);
+  }
 };
